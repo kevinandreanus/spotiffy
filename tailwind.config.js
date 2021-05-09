b3e2490ddcd31c3aea73eb45b92e1bc365c76e2e
kevinandreanus10 <kevinandreanus10@gmail.com>
2021-05-10 00:58:50 +0700

    sidebar complete

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,10 +2,24 @@ module.exports = {
   purge: [],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "spotify-300": "#121212",
+        "spotify-200": "#181818",
+        "spotify-100": "#282828",
+      },
+      lineHeight: {
+        "extra-loose": "2.5",
+      },
+      fontSize: {
+        xxs: "0.5rem",
+      },
+    },
   },
   variants: {
-    extend: {},
+    extend: {
+      textColor: ["responsive", "hover", "focus", "group-hover"],
+    },
   },
   plugins: [],
-}
+};
